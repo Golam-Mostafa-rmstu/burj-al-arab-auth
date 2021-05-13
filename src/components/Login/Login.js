7d0cee4ec713199c46b5dddd2928e887edc5224c
MostafaShelim <mostafarmstu@gmail.com>
2021-05-13 16:10:25 +0600

    login structure done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,34 @@
-import React, { useContext } from 'react';
+import { Button, FormControlLabel, FormGroup, TextField } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import { green } from '@material-ui/core/colors';
+import { withStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 
+const GreenCheckbox = withStyles({
+    root: {
+      color: green[400],
+      '&$checked': {
+        color: green[600],
+      },
+    },
+    checked: {},
+  })((props) => <Checkbox color="default" {...props} />);
 
 const Login = () => {
+    const [newUser, setNewUser] = useState(false);
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div style={{textAlign: 'center', margin: '0 auto', width: '400px', height: '400px'}}>
+            <h1>Our Email and Password Authentication</h1>
+            <FormGroup noValidate autoComplete="off">
+                <FormControlLabel
+                    control={<GreenCheckbox onChange={()=> setNewUser(!newUser)} name="checkedG" />}
+                    label="New User"
+                />
+                {newUser && <TextField type="text" id="standard-basic" label="Your Name" required/>}
+                <TextField type="email" id="standard-basic" label="Your Email" required/>
+                <TextField type="password" id="standard-basic" label="password" required/>
+                <Button style={{marginTop: '10px'}} variant="contained">{newUser ? 'Sign Up' : 'Sign In'}</Button>
+            </FormGroup>
         </div>
     );
 };
---
+++ src/components/Login/LoginManager.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDqOnkFnE1anHUbs46agR4dne2cXwVOW1A",
+    authDomain: "burj-al-arab-auth-72511.firebaseapp.com",
+    projectId: "burj-al-arab-auth-72511",
+    storageBucket: "burj-al-arab-auth-72511.appspot.com",
+    messagingSenderId: "843446847268",
+    appId: "1:843446847268:web:d1acd7cd02d727152d15b1"
+  };
