e81e2f3cd6c61bb8c32790856478e6770fbd96b2
MostafaShelim <mostafarmstu@gmail.com>
2021-05-14 23:09:18 +0600

    done create sign in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import { Button, FormControlLabel, FormGroup, TextField } from '@material-ui/core';
+import { Button, FormControlLabel, TextField } from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox';
 import { green } from '@material-ui/core/colors';
 import { withStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react';
+import { createUserWIthEmaiAndPassword, FirebaseConfig, signInWithEmailAndPassword } from './LoginManager';
+
 
 const GreenCheckbox = withStyles({
     root: {
@@ -14,21 +16,63 @@ const GreenCheckbox = withStyles({
     checked: {},
   })((props) => <Checkbox color="default" {...props} />);
 
+
 const Login = () => {
+    FirebaseConfig();
+    const [user, setUser] = useState({
+        name: '',
+        email:'',
+        password: '',
+        error: '',
+        success: false,
+    })
     const [newUser, setNewUser] = useState(false);
+    const handleFormSubmit = (e) => {
+        if(newUser && user.email && user.password){
+            createUserWIthEmaiAndPassword(user.name, user.email, user.password)
+            .then(res => setUser(res))
+            console.log(user);
+        }
+        if(!newUser && user.name && user.password){
+            signInWithEmailAndPassword(user.email, user.password)
+            .then(res => setUser(res))
+        }
+        e.preventDefault();
+
+    }
+    const googleSignIn = (e) => {
+        const userNameField = e.target.name;
+        let isFormValid = true;
+        if(userNameField === 'name'){
+            isFormValid = isFormValid && e.target.value;
+        }else if(userNameField === 'email'){
+            isFormValid = isFormValid && e.target.value;
+        }else{
+            isFormValid = isFormValid && e.target.value;
+        }
+        if(isFormValid){
+            // console.log(e.target.name, e.target.value);
+            const newUserInfo = {...user};
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
+        }
+    }
     return (
         <div style={{textAlign: 'center', margin: '0 auto', width: '400px', height: '400px'}}>
-            <h1>Our Email and Password Authentication</h1>
-            <FormGroup noValidate autoComplete="off">
+            <h1>Email and Password Authentication</h1>
+            <form autoComplete="off" onSubmit={handleFormSubmit}>
                 <FormControlLabel
                     control={<GreenCheckbox onChange={()=> setNewUser(!newUser)} name="checkedG" />}
                     label="New User"
-                />
-                {newUser && <TextField type="text" id="standard-basic" label="Your Name" required/>}
-                <TextField type="email" id="standard-basic" label="Your Email" required/>
-                <TextField type="password" id="standard-basic" label="password" required/>
-                <Button style={{marginTop: '10px'}} variant="contained">{newUser ? 'Sign Up' : 'Sign In'}</Button>
-            </FormGroup>
+                /><br></br>
+                {newUser && <TextField onBlur={googleSignIn} name="name" type="text" id="standard-basic" label="Your Name" required/>}<br></br>
+                <TextField onBlur={googleSignIn} name="email" type="email" id="standard-basic" label="Your Email" required/><br></br>
+                <TextField onBlur={googleSignIn} name="password" type="password" id="standard-basic" label="password" required/><br></br>
+
+                <Button type='submit' style={{marginTop: '10px'}} variant="contained">{newUser ? 'Sign Up' : 'Sign In'}</Button>
+            </form>
+            <p style={{color: 'red'}}>{user.error}</p>
+            {user.success && <p style={{color: 'green'}}>User {newUser ? 'created' : 'signed in'} successfully</p>}
         </div>
     );
 };
--- src/components/Login/LoginManager.js
+++ src/components/Login/LoginManager.js
@@ -1,4 +1,7 @@
-export const firebaseConfig = {
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+const firebaseConfig = {
     apiKey: "AIzaSyDqOnkFnE1anHUbs46agR4dne2cXwVOW1A",
     authDomain: "burj-al-arab-auth-72511.firebaseapp.com",
     projectId: "burj-al-arab-auth-72511",
@@ -6,3 +9,57 @@ export const firebaseConfig = {
     messagingSenderId: "843446847268",
     appId: "1:843446847268:web:d1acd7cd02d727152d15b1"
   };
+export const FirebaseConfig = () => {
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+}
+
+export const createUserWIthEmaiAndPassword = (name, email, password)=>{
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res);
+        const newUserInfo = res;
+        newUserInfo.error = '';
+        newUserInfo.success = true;
+        updateUserName(name);
+        return newUserInfo;
+    })
+    .catch((error) => {
+        var errorMessage = error.message;
+        const newUserInfo = {};
+        newUserInfo.error= errorMessage;
+        newUserInfo.success = false;
+        return newUserInfo;
+    });
+}
+
+export const signInWithEmailAndPassword = (email, password) =>{
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((res) => {
+        const newUserInfo = res;
+        newUserInfo.error = '';
+        newUserInfo.success = true;
+        return newUserInfo;
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        const newUserInfo = {};
+        newUserInfo.error = errorMessage;
+        newUserInfo.success = false;
+        return newUserInfo;
+    });
+}
+
+const updateUserName = (name) => {
+    var user = firebase.auth().currentUser;
+
+    user.updateProfile({
+    displayName: name,
+    }).then(function() {
+    // Update successful.
+    }).catch(function(error) {
+    // An error happened.
+    });
+}
